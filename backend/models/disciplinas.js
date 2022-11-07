de37075d1a269ec6f88e097b927f3b4766b2c743
giovanni.migon <giovanni.migon@tools.ages.pucrs.br>
2022-11-07 20:39:54 -0300

    first commit with schemas

---
+++ backend/models/disciplinas.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+//mongoose.connect('mongodb://localhost:27017/web-app');
+
+const Curriculo = mongoose.Schema({
+	_id: Schema.Types.ObjectId,
+	nome_curso: String,
+	semestre_inicio_vigencia: String,
+	semestre_fim_vigencia: String,
+    disciplinaCurriculo: { type: Schema.Types.ObjectId, ref: 'Disciplina' },
+});
+
+const Disciplina = new Schema({
+	_id: Schema.Types.ObjectId,
+	nome: String,
+    creditos: Number,
+    ementa: String,
+    requisito: { type: Schema.Types.ObjectId, ref: 'Disciplina' },
+    semestre: { type: Schema.Types.ObjectId, ref: 'Curriculo' },
+});
+
+module.exports = mongoose.model("CurriculoInstance", Curriculo);
+module.exports = mongoose.model("DisciplinaInstance", Disciplina);
