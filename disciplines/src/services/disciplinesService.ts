6b7ed5aa8307ec5091ca837feb0865bb06c88a20
Draf <fontanadias@gmail.com>
2022-11-27 19:12:54 -0300

    alter services

--- disciplines/src/services/disciplinesService.ts
+++ disciplines/src/services/disciplinesService.ts
@@ -2,6 +2,7 @@ import { Types } from "mongoose";
 import IDiscipline from "../interfaces/IDiscipline";
 import Discipline from "../models/Discipline"
 
+
 export async function getAll(attributes: object) {
   if (attributes) {
     return Discipline.find(attributes).populate("discipline");
@@ -14,21 +15,25 @@ export async function getById(id: string) {
   return await Discipline.findById(id).populate("discipline");
 }
 
+
 export async function create(discipline: IDiscipline) {
-  return await Discipline.create(discipline);
+  const createdDiscipline = await Discipline.create(discipline);
+  return createdDiscipline;
 }
 
-export async function addDiscipline(
-  disciplineId: Types.ObjectId
-) {
-  const discipline = await getById(disciplineId.toString());
-  if (discipline) {
-    discipline.push(disciplineId);
-    await discipline.save();
-  } else {
-    throw new Error("discipline not found.");
-  }
-}
+// export async function addDiscipline(
+//   disciplineId: Types.ObjectId
+//   const createDiscipline = await Discipline.create(discipline);
+// ) {
+//   const discipline = await getById(disciplineId.toString());
+//   if (discipline) {
+
+//     discipline.push(disciplineId);
+//     await discipline.save();
+//   } else {
+//     throw new Error("discipline not found.");
+//   }
+// }
 
 export async function updateById(id: string, discipline: IDiscipline) {
   return await Discipline.findByIdAndUpdate(id, discipline);
