b171de0f175544801c83dce1138a8acdf25207d5
Draf <fontanadias@gmail.com>
2022-11-27 19:32:17 -0300

    alter databese file

--- disciplines/src/database/database.ts
+++ disciplines/src/database/database.ts
@@ -17,7 +17,8 @@ const url = `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}:${MON
 //const url = `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}:${MONGODB_PORT}/${DATABASE_NAME}?authSource=admin`;
 export async function startDatabase() {
   try {
-    mongodb://user:password@my-shard-00.atlas.mongodb.net:27017
+    url;
+    //mongodb://user:password@my-shard-00.atlas.mongodb.net:27017
     //await mongoose.connect("mongodb://admin:a12345678@mongo:27017");
     await mongoose.connect(url);
     await Discipline.createCollection();
