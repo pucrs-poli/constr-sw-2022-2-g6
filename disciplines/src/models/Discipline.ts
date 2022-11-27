30312b2859d5c833c76f962ecf0e4f580ba335c6
Draf <fontanadias@gmail.com>
2022-11-27 18:48:30 -0300

    fix models

--- disciplines/src/models/Discipline.ts
+++ disciplines/src/models/Discipline.ts
@@ -1,32 +1,19 @@
+import { number } from "joi";
 import mongoose from "mongoose";
 import IDiscipline from "../interfaces/IDiscipline";
 
 
 const disciplineSchema = new mongoose.Schema<IDiscipline>({
-    id: {
-        type: String,
-        required: true,
-    },
-    name: {
-        type: String,
-        required: true,
-    },
-    credits: {
-      type: Number,
-      required: true,
-    },
-    syllabus: {
-      type: String,
-      required: true,
-    },
-    requisite: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Discipline'
-    },
-    semester: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Curriculum'
-      },
+    id: {type: number, required: true}, //codigo da disciplina
+    cod: {type: String, required: true},
+    name: {type: String, required: true},
+    credits: {type: Number, required: true},
+    valid: {type: String, required: true},
+    workload: {type: number, require: true},
+    goals: {type: String, require: true},
+    syllabus: {type: String,required: true}, //ementa da disciplina
+    //requisite: {type: mongoose.Schema.Types.ObjectId, ref: 'Discipline'},
+    //semester: {type: mongoose.Schema.Types.ObjectId, ref: 'Curriculum'}, //validade
   },
 );
 
