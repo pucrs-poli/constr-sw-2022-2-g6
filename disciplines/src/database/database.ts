c5b459576e03c25f0e4e6fb2c7f93b66cc43a60a
Draf <fontanadias@gmail.com>
2022-11-29 23:37:29 -0300

    docker/compose and database files

--- disciplines/src/database/database.ts
+++ disciplines/src/database/database.ts
@@ -14,11 +14,8 @@ import mongoose from "mongoose";
 
 const DATABASE_NAME = "disciplines";
 const url = `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}:/${DATABASE_NAME}?authSource=admin`;
-//const url = `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}:${MONGODB_PORT}/${DATABASE_NAME}?authSource=admin`;
 export async function startDatabase() {
   try {
-    //mongodb://user:password@my-shard-00.atlas.mongodb.net:27017
-    //await mongoose.connect("mongodb://admin:a12345678@mongo:27017");
     await mongoose.connect(url);
     await Discipline.createCollection();
     console.log("[OK] Building collection.");
